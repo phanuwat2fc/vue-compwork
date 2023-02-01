86fcc8d65a78a4ad6ad9f5e941edc0ce961c9f31
Papawin.S <papawin085087@gmail.com>
2023-02-01 21:03:25 +0700

    django get post

--- django/myapp/urls.py
+++ django/myapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/',views.hello, name = 'hello',),
-    path('', views.index, name = 'index'),
+    
 ]
--- django/myapp/views.py
+++ django/myapp/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+
 
 # Create your views here.
-
-def hello(request):
-    return HttpResponse('Hello World')
-
-def index(request):
-    id = ''
-    even = {
-        'GG',
-        'Good',
-        'ping',
-        }
-
-    return render(request, 'index.html', {
-        'id': id,
-        'even': even,
-    })
