c7f58a61021807c06572c1786a36fbe211b391b7
Phanuwat <biglove1320@gmail.com>
2023-06-26 21:48:57 +0700

    commit last

--- compwork002/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-import router from './router'
-
-createApp(App).use(Quasar, quasarUserOptions,router).mount('#app')
